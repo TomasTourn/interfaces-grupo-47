c5fe29eed64d171c21f6e33c725c344a63c6fc92
Tomás Tourn <tomytourn1@gmail.com>
2024-09-25 13:57:41 -0300

    boton y dots slider

--- TP2/EntregaFinal/js/slider.js
+++ TP2/EntregaFinal/js/slider.js
@@ -1,46 +1,57 @@
-document.getElementById('prev').onclick=function(event){
-    event.preventDefault();
-    plusSlides(-1);
-}
-document.getElementById('next').onclick=function(event){
-    event.preventDefault();
-    plusSlides(1);
-}
-for (let i = 1; i <= 3; i++) {
-    document.getElementById(`dot${i}`).onclick = function(event) {
-        event.preventDefault();
-        currentSlides(i);
-    };
+let currentSlide=0;
+const slides= document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+const totalSlides = slides.length;
+const buttons=document.querySelectorAll('.slider-button');
+
+
+document.querySelector('.next').addEventListener('click', () => {
+    changeSlide(currentSlide+1);
+  });
+  
+  document.querySelector('.prev').addEventListener('click', () => {
+    changeSlide(currentSlide-1);
+  });
+
+dots.forEach((dot,index)=>{
+    dot.addEventListener('click',()=>{
+        changeSlide(index);
+    })
+})
+
+buttons.forEach(button=>{
+    button.addEventListener('click',function(){
+        const disabled = this.getAttribute('data-disabled');
+        if(disabled===null){
+            const url= this.getAttribute('data-url');
+            window.location.href=url;
+        }
+    })
+})
+
+function changeSlide(index){
+    if(index>=totalSlides){
+        currentSlide=0;
+    }
+    else if(index<0){
+        currentSlide=totalSlides-1;
+    }
+    else{
+        currentSlide=index;
+    }
+    document.querySelector('.slides').style.transform=`translateX(-${currentSlide*100}%)`;
+
+    slides.forEach((slide,i)=>{
+        slide.classList.toggle('active',i===currentSlide);
+    })
+
+    dots.forEach((dot,i)=>{
+        dot.classList.toggle('active',i===currentSlide);
+})
 }
 
-let slideIndex=1;
-showSlides(slideIndex);
 
-function plusSlides(n){
-    showSlides(slideIndex+=n);
-}
 
-function currentSlide(n){
-    showSlides(slideIndex = n)
-}
 
-function showSlides(n){
-    let i;
-    let slides=document.querySelectorAll('.slides');
-    let dots=document.querySelectorAll('.dot');
-    
-    if(n > slides.length){
-        slideIndex =1
-    }
-    if(n < 1){
-        slideIndex=slides.length;
-    }
-    for(i=0;i < slides.length; i++){
-        slides[i].style.display="none";
-    }
-    for(i=0;i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex -1].style.display="block";
-    dots[slideIndex -1].className+=" active";
-}
+
+
