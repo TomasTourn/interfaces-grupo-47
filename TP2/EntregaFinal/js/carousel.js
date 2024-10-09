c13e220ebfb7cdc964d367795ec0aaf791563105
Tomás Tourn <tomytourn1@gmail.com>
2024-10-09 17:05:51 -0300

    carousel working

--- TP2/EntregaFinal/js/carousel.js
+++ TP2/EntregaFinal/js/carousel.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
     const carouselContainers = document.querySelectorAll('.carousel-container');
-    let currentIndex=0;
     const cardWidth = document.querySelector('.card').offsetWidth + 15; // Ancho de la card + margen
 
     
@@ -12,39 +11,51 @@ document.addEventListener('DOMContentLoaded',()=>{
             const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
 
             if (carousel.scrollLeft >= maxScrollLeft) {
+                addSkew();
                 carousel.scrollLeft = 0;
+               
             } else {
-                carousel.scrollLeft += cardWidth;
+                addSkew();
+                  carousel.scrollLeft += cardWidth;
+
             }
+
+            
         });
 
         container.querySelector('.left-arrow').addEventListener('click', () => {
             if (carousel.scrollLeft <= 0) {
+                addSkew();
                 carousel.scrollLeft = carousel.scrollWidth - carousel.clientWidth;
             } else {
+               
                 carousel.scrollLeft -= cardWidth;
+                addSkew();
             }
         });
 
+
+function addSkew(){
+                carousel.querySelectorAll('.card').forEach(card=>{
+                    card.classList.add("addSkew");
+                })
+                
+
+                setTimeout(() => {
+                    carousel.querySelectorAll('.card').forEach(card => {
+                      card.classList.add("addSkewEnd");
+                    });
+                  }, 300);
+
+                  setTimeout(() => {
+                    carousel.querySelectorAll('.card').forEach(card => {
+                      card.classList.remove("addSkewEnd");
+                      card.classList.remove("addSkew");
+                    });
+                  }, 600);
+            }
+
+
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })
