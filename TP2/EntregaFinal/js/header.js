4faf77b09d3796b12af1dc01d3d453ff5985fe73
Francisco Parra <98franciscoparra@gmail.com>
2024-10-11 15:56:28 -0300

    Animacion de carga

--- TP2/EntregaFinal/js/header.js
+++ TP2/EntregaFinal/js/header.js
@@ -1,23 +1,61 @@
-const menuBtn = document.querySelector('.menu-button');
-const sidebar = document.querySelector('.sidebar');
-const closeMenu = document.querySelector('.icon-cruz-menu');
+document.addEventListener('DOMContentLoaded',()=>{
 
-const profileBtn = document.querySelector('.user-icon');
-const profile = document.querySelector('.user-menu');
-const closeProfile = document.querySelector('.icon-cruz-profile');
+    const menuBtn = document.querySelector('.menu-button');
+    const sidebar = document.querySelector('.sidebar');
+    const closeMenu = document.querySelector('.icon-cruz-menu');
+    
+    const profileBtn = document.querySelector('.user-icon');
+    const profile = document.querySelector('.user-menu');
+    const closeProfile = document.querySelector('.icon-cruz-profile');
+    
+    menuBtn.addEventListener('click', () => {
+        sidebar.classList.toggle('show');
+    });
+    
+    closeMenu.addEventListener('click', ()=>{
+        sidebar.classList.toggle('show');
+    });
+    
+    profileBtn.addEventListener('click', ()=>{
+        profile.classList.toggle('show');
+    });
+    
+    closeProfile.addEventListener('click', ()=>{
+        profile.classList.toggle('show');
+    });
+    
+    //animacion de carga de pagina
 
-menuBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('show');
-});
+    var percent = document.querySelector('.percent');
+    var progress = document.querySelector('.progress');
+    var count = 4;
+    var per = 16;
+    var loading = setInterval(animate, 50);
 
-closeMenu.addEventListener('click', ()=>{
-    sidebar.classList.toggle('show');
-});
+    function animate(){
+    if(count >= 100 && per >= 196){ 
+        count = 100; // Aseguramos que count no pase de 100
+        per = 196; // Aseguramos que per no pase de 196
+        progress.style.width = per + 'px';
+        percent.textContent = count + '%';
+        text.style.fontSize = "70px";
+        text.classList.add("add");
+        clearInterval(loading);
+    } else {
+        per = per + 1.8;
+        count = count + 1;
+        if(count > 100) count = 100; // Limitar count a 100
+        progress.style.width = per + 'px';
+        percent.textContent = count + '%';
+    }
+    }
 
-profileBtn.addEventListener('click', ()=>{
-    profile.classList.toggle('show');
-});
+    setTimeout(function() {
+        document.querySelector('.loader-container').style.display = 'none';
+    }, 5000); // 5000 ms = 5 segundos
+    
+    setTimeout(function() {
+        document.querySelector('.page-container').style.display = 'block';
+    }, 5000);
 
-closeProfile.addEventListener('click', ()=>{
-    profile.classList.toggle('show');
-});
+    })
