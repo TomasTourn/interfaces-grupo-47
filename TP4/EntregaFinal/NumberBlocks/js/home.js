f27823f6575a1e7c830a7854ad2653e4107be15f
Tomás Tourn <tomytourn1@gmail.com>
2024-11-13 19:15:18 -0300

    maqueta primeras 3 secciones

--- TP4/EntregaFinal/NumberBlocks/js/home.js
+++ TP4/EntregaFinal/NumberBlocks/js/home.js
@@ -2,9 +2,25 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
 
+    window.addEventListener('scroll',()=>{
+        let logo=document.querySelector('.logo-img');
+        let header=document.querySelector('.nav-header');
+        let scrollPosition= window.scrollY;
+    
+        let scaleFactor=Math.max(0.5,1 - scrollPosition/500);
+
+        if(scrollPosition==0){
+            logo.style.transform=`translate(0,0) scale(1)`
+            header.style.background= 'transparent';
+        }
+        else{
+         
+            logo.style.transform=`translate(0,-50%) scale(${scaleFactor})`
+            header.style.background='linear-gradient(180deg, #00D1D5 0%, rgba(0, 209, 213, 0.12) 87.91%, rgba(1, 208, 213, 0) 100%)';
+        }
 
 
+    })
 
-
-
+   
 })
