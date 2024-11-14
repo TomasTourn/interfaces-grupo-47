b5132e94a0b6b965695b90481f17d96f361478ef
micadiazz1 <micaelaexactas1@gmail.com>
2024-11-14 15:57:31 -0300

    seccion 4 hice el punto 8 de mousemove con la imagen

--- TP4/EntregaFinal/NumberBlocks/js/home.js
+++ TP4/EntregaFinal/NumberBlocks/js/home.js
@@ -19,8 +19,28 @@ document.addEventListener("DOMContentLoaded",()=>{
             header.style.background='linear-gradient(180deg, #00D1D5 0%, rgba(0, 209, 213, 0.12) 87.91%, rgba(1, 208, 213, 0) 100%)';
         }
 
+        (function() {
+            document.addEventListener("mousemove", moveCharacter);
+            const elem = document.querySelector(".img-conjuntoPersonajes");
+        
+            function moveCharacter(e) {
+                let w = window.innerWidth /2
+                let h = window.innerHeight / 2;
+                let mouseX = e.clientX;
+                let mouseY = e.clientY;
+        
+                
+                let depth1 = -(mouseX - w) * 0.10;
+                let depth2 = -(mouseY - h) * 0.10;
+                
+
+                elem.style.transform = `translate3d(${depth1}px, ${depth2}px, 0) scale(1.2)`;
+            }
+        })();
 
     })
+
+
 
    
 })
