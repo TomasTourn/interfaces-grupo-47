a95ec6e7f5994b344b577483a3787946e24fd820
Tomás Tourn <tomytourn1@gmail.com>
2024-11-14 15:39:28 -0300

    hamburguesa x

--- TP4/EntregaFinal/NumberBlocks/js/home.js
+++ TP4/EntregaFinal/NumberBlocks/js/home.js
@@ -18,9 +18,25 @@ document.addEventListener("DOMContentLoaded",()=>{
             logo.style.transform=`translate(0,-50%) scale(${scaleFactor})`
             header.style.background='linear-gradient(180deg, #00D1D5 0%, rgba(0, 209, 213, 0.12) 87.91%, rgba(1, 208, 213, 0) 100%)';
         }
-
-
     })
 
+    let hamburguesa=document.querySelector('.hamburguesa');
+        hamburguesa.addEventListener('click',()=>{
+            hamburguesa.classList.toggle('open');
+            let data=hamburguesa.getAttribute('data-open');
+            if(data==false){
+                hamburguesa.classList.add('notOpen');
+                hamburguesa.classList.remove('Open');
+                hamburguesa.setAttribute('data-open', 'true');
+            }
+            else{
+                hamburguesa.classList.add('Open');
+                hamburguesa.classList.remove('notOpen');
+                hamburguesa.setAttribute('data-open', 'false');
+            }
+            document.querySelector('.hamburguesa-bg').classList.toggle("change-bg");
+        })
+
+    
    
 })
