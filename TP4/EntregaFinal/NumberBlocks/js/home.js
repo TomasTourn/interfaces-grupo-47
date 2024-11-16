d305f798e71ba85f1dc3a28ab140b9802fd29659
Tomás Tourn <tomytourn1@gmail.com>
2024-11-16 01:43:52 -0300

    parallax home

--- TP4/EntregaFinal/NumberBlocks/js/home.js
+++ TP4/EntregaFinal/NumberBlocks/js/home.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded",()=>{
         let header=document.querySelector('.nav-header');
         let scrollPosition= window.scrollY;
     
-        let scaleFactor=Math.max(0.5,1 - scrollPosition/500);
+        let scaleFactor=Math.max(0.5,1 - scrollPosition/100);
 
         if(scrollPosition==0){
             logo.style.transform=`translate(0,0) scale(1)`
@@ -25,4 +25,49 @@ document.addEventListener("DOMContentLoaded",()=>{
             hamburguesa.classList.toggle('active');
             document.querySelector('.opciones-hamburguesa').classList.toggle("activa");
         })
+
+        /*parallax*/
+
+        const arbol1 = document.querySelector('.arbol-1');
+        const arbol2 = document.querySelector('.arbol-2');
+        const arbol3 = document.querySelector('.arbol-3');
+
+        const number1 = document.querySelector('.number-1-home');
+        const number2 = document.querySelector('.number-2-home');
+        const number3 = document.querySelector('.number-3-home');
+
+        const bush1 = document.querySelector('.arbusto-1');
+        const bush2 = document.querySelector('.arbusto-2');
+        const bush3 = document.querySelector('.arbusto-3');
+        const bush4 = document.querySelector('.arbusto-4');
+
+        const rock1 = document.querySelector('.piedra-1');
+        const rock2 = document.querySelector('.piedra-2');
+        const rock3 = document.querySelector('.piedra-3');
+        const rock4 = document.querySelector('.piedra-4');
+
+        window.addEventListener('scroll',()=>{
+            const scrollY=window.scrollY;
+
+            arbol1.style.transform=`translateX(${scrollY * 0.2}px)`
+            arbol2.style.transform=`translateX(${-scrollY * 0.15}px)`
+       
+            arbol3.style.transform=`translateX(${-scrollY * 0.09}px)`
+
+            number1.style.transform=`translateX(${-scrollY * 0.2}px) translateY(${-scrollY * 0.35}px`//translateX(${-scrollY * 0.13}px)
+            number2.style.transform=`translateX(${scrollY * 0}px) translateY(${-scrollY * 0.28}px)`//translateX(${-scrollY * 0.13}px)
+            number3.style.transform=`translateX(${scrollY * 0.15}px) translateY(${-scrollY * 0.15}px) `//translateX(${scrollY * 0.07}px)
+
+            bush1.style.transform=`translateX(${scrollY * 0.1}px)`
+            bush2.style.transform=`translateX(${scrollY * 0.23}px)`
+            bush3.style.transform=`translateX(${-scrollY * 0.15}px)`
+            bush4.style.transform=`translateX(${-scrollY * 0.05}px)`
+
+            rock1.style.transform=`translateX(${scrollY * 0.215}px)`
+            rock2.style.transform=`translateX(${-scrollY * 0.21}px)`
+            rock3.style.transform=`translateX(${-scrollY * 0.17}px)`
+            rock4.style.transform=`translateX(${-scrollY * 0.19}px)`
+
+        })
+
 })
