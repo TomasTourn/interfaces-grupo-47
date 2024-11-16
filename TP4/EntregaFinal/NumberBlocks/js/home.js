8ea82c63aa1af820b24930e564aaf5e57b01c220
micadiazz1 <micaelaexactas1@gmail.com>
2024-11-16 13:48:28 -0300

    agregue en la seccion 4 el mouseMove y en la seccion 6 agregue el desplazamiento

--- TP4/EntregaFinal/NumberBlocks/js/home.js
+++ TP4/EntregaFinal/NumberBlocks/js/home.js
@@ -68,6 +68,42 @@ document.addEventListener("DOMContentLoaded",()=>{
             rock3.style.transform=`translateX(${-scrollY * 0.17}px)`
             rock4.style.transform=`translateX(${-scrollY * 0.19}px)`
 
-        })
+        });
+        window.addEventListener('scroll',()=>{
+            let seccion6 = document.querySelector('.container-section-6');
+            let personaje3 = document.querySelector('.img-personaje3Video');
+            let video = document.querySelector('.video');
+    
+            let inicioScrollSeccion6 = seccion6.offsetTop;
+            let scrollY = window.scrollY;
+    
+            if (scrollY >= inicioScrollSeccion6) {
+                let desplazamiento = (scrollY - inicioScrollSeccion6-300) * 0.4;
+                let desplazamientovideo = (scrollY - inicioScrollSeccion6-300) * 0.1;
+                personaje3.style.transform = `translateY(-${desplazamiento}px)`;
+                video.style.transform = `translateY(${desplazamientovideo}px)`; 
+            }
+        });
+
+        (function() {
+            document.addEventListener("mousemove", moveCharacter);
+            const elem = document.querySelector(".img-conjuntoPersonajes");
+        
+            function moveCharacter(e) {
+                let w = window.innerWidth /2
+                let h = window.innerHeight / 2;
+                let mouseX = e.clientX;
+                let mouseY = e.clientY;
+        
+                
+                let depth1 = -(mouseX - w) * 0.10;
+                let depth2 = -(mouseY - h) * 0.10;
+                
+    
+                elem.style.transform = `translate3d(${depth1}px, ${depth2}px, 0) scale(1.2)`;
+            }
+        })();
+
+
 
 })
